48e2acfe1ea7706f80064f626fd142f283b14aeb
caney93 <can.eren93@yahoo.de>
2026-04-16 13:31:38 +0200

    feat: Spieler bewegt sich mit Pfeiltasten

---
+++ player.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+var speed = 500
+
+func _process(delta):
+	var direction = Vector2.ZERO
+	if Input.is_action_just_pressed("ui_right"):
+		direction.x += 1
+	if Input.is_action_just_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_just_pressed("ui_down"):
+		direction.y += 1
+	if Input.is_action_just_pressed("ui_up"):
+		direction.y -= 1
+	position += direction.normalized() * speed * delta
