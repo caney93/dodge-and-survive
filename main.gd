a97147b0bde58c137573e8d5a8cca8ccfc08243b
caney93 <can.eren93@yahoo.de>
2026-04-16 20:28:57 +0200

    feat: Score-Anzeige läuft

--- main.gd
+++ main.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
 var obstacle_scene = preload("res://obstacle.tscn")
+var score = 0
+
+@onready var score_label = $ScoreLabel
+
+func _process(delta):
+	score += delta
+	score_label.text = "Score: "  + str(int(score))
 
 func _on_spawn_timer_timeout():
 	var obs = obstacle_scene.instantiate()
