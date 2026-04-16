ef5269c64a386e20f1c8b4c21f51581d1e087303
caney93 <can.eren93@yahoo.de>
2026-04-16 20:43:57 +0200

    feat: Schwierigkeitskurve und Global-Score

--- main.gd
+++ main.gd
@@ -7,7 +7,10 @@ var score = 0
 
 func _process(delta):
 	score += delta
+	Global.score = score
 	score_label.text = "Score: "  + str(int(score))
+	$SpawnTimer.wait_time = max(0.1, 1.0 - score * 0.02)
+	
 
 func _on_spawn_timer_timeout():
 	var obs = obstacle_scene.instantiate()
